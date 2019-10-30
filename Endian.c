deac275e9f7775e3969b339dfa09f2a12d96377b
Gaurav-Khairmode <57167440+Gaurav-Khairmode@users.noreply.github.com>
2019-10-30 21:56:04 +0530

    Update Endian.c

--- Endian.c
+++ Endian.c
@@ -3,7 +3,7 @@ unsigned int LitToBigEndian(unsigned int x);
 
 int main( )
 {
-	int Little_Endian = 0xAABBCCDD ;
+	int c, Little_Endian = 0xAABBCCDD ;
 
 	printf("\n Little_Endian = 0x%X\n",Little_Endian);
 
@@ -13,5 +13,6 @@ return 0;
 
 unsigned int LitToBigEndian(unsigned int x)
 {
-	return (((x>>24) & 0x000000ff) | ((x>>8) & 0x0000ff00) | ((x<<8) & 0x00ff0000) | ((x<<24) & 0xff000000));
+	c= (((x>>24) & 0x000000ff) | ((x>>8) & 0x0000ff00) | ((x<<8) & 0x00ff0000) | ((x<<24) & 0xff000000));
+	return c;
 }
